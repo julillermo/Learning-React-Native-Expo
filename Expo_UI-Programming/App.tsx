12db384104ebb52637aabf4d9df4d1902c762332
julillermo <julius.guillermo.work@gmail.com>
2024-05-12 17:22:05 +0800

    finish up to 'Display a popup toast' of the Expo docs

--- Expo_UI-Programming/App.tsx
+++ Expo_UI-Programming/App.tsx
@@ -11,6 +11,9 @@ import {
   useState,
 } from 'react';
 import { Ionicons } from '@expo/vector-icons'
+import SVG, { Path } from "react-native-svg"
+import { RootSiblingParent } from 'react-native-root-siblings'
+import Toast from 'react-native-root-toast';
 
 const image = { uri: "https://docs.expo.dev/static/images/tutorial/splash.png" };
 
@@ -27,6 +30,14 @@ function MyCheckbox({ onChange, checked }: {onChange: (event: GestureResponderEv
 
 export default function App() {
   const [checked, setChecked] = useState(false);
+
+  const displayToast = () => {
+    const toast = Toast.show('Toast message appears here', {duration: Toast.durations.LONG})
+    const hideToast = () => {
+      Toast.hide(toast);
+    }
+    setTimeout(hideToast, 3000)
+  }
 
   return (
     /* ELEMENT IN FRONT OF BACKGROUND */
@@ -48,69 +59,101 @@ export default function App() {
     // </View>
 
     /* STACK VIEWS WITH ZINDEX */
-    <View style={styles.root}>
-      <View
-        style={[
-          styles.containerZIndex,
-          {
-            backgroundColor: '#e1e4e8',
-          }
-        ]}>
+    // <View style={styles.root}>
+    //   <View
+    //     style={[
+    //       styles.containerZIndex,
+    //       {
+    //         backgroundColor: '#e1e4e8',
+    //       }
+    //     ]}>
 
-        {/* zIndex:0 */}
-        <View style={[styles.item, { backgroundColor: '#6638f0' }]}>
-          <Text style={styles.itemLabel}>zIndex:0</Text>
-        </View>
+    //     {/* zIndex:0 */}
+    //     <View style={[styles.item, { backgroundColor: '#6638f0' }]}>
+    //       <Text style={styles.itemLabel}>zIndex:0</Text>
+    //     </View>
 
-        {/* zIndex:1 */}
-        <View style={[styles.item,
-          {
-            backgroundColor: '#5cc9f5',
-            position: 'absolute',
-            top: 32,
-            left: 32,
-            zIndex: 1,
-          }]}>
-          <Text style={styles.itemLabel}>zIndex:1</Text>
-        </View>
+    //     {/* zIndex:1 */}
+    //     <View style={[styles.item,
+    //       {
+    //         backgroundColor: '#5cc9f5',
+    //         position: 'absolute',
+    //         top: 32,
+    //         left: 32,
+    //         zIndex: 1,
+    //       }]}>
+    //       <Text style={styles.itemLabel}>zIndex:1</Text>
+    //     </View>
 
-        {/* zIndex:-1 */}
-        <View style={[styles.item,
-          {
-            backgroundColor: '#4af2a1',
-            marginTop: -16,
-            zIndex: -1,
-          }]}>
-          <Text style={[styles.itemLabel, {color: 'black'}]}>zIndex:-1</Text>
-        </View>
+    //     {/* zIndex:-1 */}
+    //     <View style={[styles.item,
+    //       {
+    //         backgroundColor: '#4af2a1',
+    //         marginTop: -16,
+    //         zIndex: -1,
+    //       }]}>
+    //       <Text style={[styles.itemLabel, {color: 'black'}]}>zIndex:-1</Text>
+    //     </View>
 
-        {/* zIndex:2 */}
-        {/* You can even make the component extend outside of the parent's visual bounds */}
-        <View style={[styles.item,
-          {
-            backgroundColor: '#dfc73f',
-            top: -160,
-            left: -64,
-            zIndex: 2,
-          }]}>
-          <Text style={[styles.itemLabel, {color: 'black'}]}>zIndex:2</Text>
-        </View>
+    //     {/* zIndex:2 */}
+    //     {/* You can even make the component extend outside of the parent's visual bounds */}
+    //     <View style={[styles.item,
+    //       {
+    //         backgroundColor: '#dfc73f',
+    //         top: -160,
+    //         left: -64,
+    //         zIndex: 2,
+    //       }]}>
+    //       <Text style={[styles.itemLabel, {color: 'black'}]}>zIndex:2</Text>
+    //     </View>
 
-        {/* zIndex:-2 */}
-        {/* An absolute position must still respect the zIndex*/}
-        {/* But the elements are still with respect the and contained within the
-          zIndex of their parent container */}
-        <View style={[styles.item,
-          {
-            backgroundColor: '#3fdfcc',
-            top: -192,
-            left: -32,
-            zIndex: 1,
-          }]}>
-          <Text style={[styles.itemLabel, {color: 'black'}]}>zIndex:-2</Text>
-        </View>
+    //     {/* zIndex:-2 */}
+    //     {/* An absolute position must still respect the zIndex*/}
+    //     {/* But the elements are still with respect the and contained within the
+    //       zIndex of their parent container */}
+    //     <View style={[styles.item,
+    //       {
+    //         backgroundColor: '#3fdfcc',
+    //         top: -192,
+    //         left: -32,
+    //         zIndex: 1,
+    //       }]}>
+    //       <Text style={[styles.itemLabel, {color: 'black'}]}>zIndex:-2</Text>
+    //     </View>
+    //   </View>
+    // </View>
+
+    /* Use SVGs */
+    // <View style={styles.containerSVG}>
+    //   <SVG
+    //     width={20}
+    //     height={20}
+    //     viewBox="0 0 20 20"
+    //   >
+    //     <Path d="M16.993 6.667H3.227l6.883 6.883 6.883-6.883z" fill="#000" />
+    //   </SVG>
+    // </View>
+
+    /* Display a popup toast */
+    <RootSiblingParent>
+      {/**
+       * React-Native standaard library only supports toast for Android but, the
+       * third-party react-native-root-toast allows for cross-platform toast messages.
+       *
+       * iOS systems don't actually have any native toast functionality.
+       *
+       * The root component must be wrapped by <RootSiblingParent>
+       */}
+      <View style={styles.containerSVG}>
+        <Pressable
+          style={{ padding: 10, borderWidth: 1, borderColor: "blue", borderRadius: 12 }}
+          onLongPress={displayToast}
+        >
+          <Text>Toast Test (long press)</Text>
+        </Pressable>
       </View>
-    </View>
+    </RootSiblingParent>
+
   );
 }
 
@@ -169,7 +212,6 @@ const styles = StyleSheet.create({
   },
 
   /* STACK VIEWS WITH ZINDEX */
-
   root: {
     flex: 1,
     backgroundColor: "#fff",
@@ -196,5 +238,12 @@ const styles = StyleSheet.create({
     color: 'white',
     alignItems: 'center',
     justifyContent: 'space-around',
+  },
+
+  /* Use SVGs */
+  containerSVG: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
   }
 });
